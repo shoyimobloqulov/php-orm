a306d0cb06362cb92258bbd136b863585fead85a
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2024-05-13 13:15:34 +0500

    sql filter and get client ip function

--- src/DataBaseORM.php
+++ src/DataBaseORM.php
@@ -35,4 +35,30 @@
         public function disconnect() {
             mysqli_close($this->conn);
         }
+
+        public function filter($var)
+        {
+            $var = trim($var);
+            $var = stripslashes($var);
+            return htmlentities($var);
+        }
+
+        public function get_client_ip() {
+            $ipaddress = '';
+            if (getenv('HTTP_CLIENT_IP'))
+                $ipaddress = getenv('HTTP_CLIENT_IP');
+            else if(getenv('HTTP_X_FORWARDED_FOR'))
+                $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+            else if(getenv('HTTP_X_FORWARDED'))
+                $ipaddress = getenv('HTTP_X_FORWARDED');
+            else if(getenv('HTTP_FORWARDED_FOR'))
+                $ipaddress = getenv('HTTP_FORWARDED_FOR');
+            else if(getenv('HTTP_FORWARDED'))
+                $ipaddress = getenv('HTTP_FORWARDED');
+            else if(getenv('REMOTE_ADDR'))
+                $ipaddress = getenv('REMOTE_ADDR');
+            else
+                $ipaddress = 'UNKNOWN';
+            return $ipaddress;
+        }
     }
