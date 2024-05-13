a56f722f167fb0c59f05baa74418761d868aec27
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2024-05-13 13:03:43 +0500

    database orm connect and query method add

--- src/DataBaseORM.php
+++ src/DataBaseORM.php
@@ -1,15 +1,31 @@
 <?php
     namespace Shoyim\ORM;
 
+    use PDO;
+
     abstract class DataBaseORM
     {
-        protected $conn;
-        protected $user;
-        protected $db_name;
-        protected $password;
+        private $host;
+        private $user;
+        private $pass;
+        private $dbname;
+        private $pdo;
 
-        public function __construct()
-        {
-
+        public function __construct($host, $user, $pass, $dbname) {
+            $this->host = $host;
+            $this->user = $user;
+            $this->pass = $pass;
+            $this->dbname = $dbname;
         }
+        public function connect() {
+            $dsn = "mysql:host=$this->host;dbname=$this->dbname";
+            $this->pdo = new PDO($dsn, $this->user, $this->pass);
+        }
+        public function query($sql, $params = []) {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt;
+        }
+
+
     }
